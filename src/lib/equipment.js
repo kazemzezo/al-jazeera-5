92cc67926fa42b131f97597fba369fa46154fcb5
kazemzezo <kazemzezo80@gmail.com>
2026-09-09 15:19:43 +0300

    Create equipment.js

---
+++ src/lib/equipment.js
@@ -0,0 +1,8 @@
+export const EQUIPMENT = [
+  { id: "kabbash", name: "الكباش", pricePerHour: 2000 },
+  { id: "crawler_crane", name: "رافعه مجنزره 30 طن", pricePerHour: 1600 },
+  { id: "forklift", name: "رافعه شوكيه 11 طن", pricePerHour: 1200 },
+];
+
+export const WORKER_PRICE_PER_CAR = 200;
+export const TOTAL_WORKERS = 12;
