19c44dae5842f0cb77cf7ca7eba394e7b33760ae
kazemzezo <kazemzezo80@gmail.com>
2026-09-15 08:28:03 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { GuestPromptProvider } from "./context/GuestPromptContext";
 import { ThemeProvider } from "./context/ThemeContext";
+import { ToastProvider } from "./context/ToastContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 import AppLayout from "./components/AppLayout";
 import Login from "./pages/Login";
@@ -21,62 +22,64 @@ import { ROLES, DRIVER_ROLES } from "./lib/roles";
 export default function App() {
   return (
     <ThemeProvider>
-      <BrowserRouter basename="/al-jazeera-5">
-        <AuthProvider>
-          <GuestPromptProvider>
-            <Routes>
-              <Route path="/login" element={<Login />} />
+      <ToastProvider>
+        <BrowserRouter basename="/al-jazeera-5">
+          <AuthProvider>
+            <GuestPromptProvider>
+              <Routes>
+                <Route path="/login" element={<Login />} />
 
-              <Route element={<AppLayout />}>
-                <Route path="/" element={<Home />} />
-                <Route path="/dock" element={<Dock />} />
-                <Route path="/yard" element={<Yard />} />
-                <Route path="/ads/:id" element={<AdDetails />} />
-                <Route path="/calculator" element={<Calculator />} />
-                <Route path="/experimental" element={<Experimental />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/guide" element={<Guide />} />
-                <Route path="/terms" element={<Terms />} />
-                <Route path="/privacy" element={<Privacy />} />
+                <Route element={<AppLayout />}>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/dock" element={<Dock />} />
+                  <Route path="/yard" element={<Yard />} />
+                  <Route path="/ads/:id" element={<AdDetails />} />
+                  <Route path="/calculator" element={<Calculator />} />
+                  <Route path="/experimental" element={<Experimental />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/contact" element={<Contact />} />
+                  <Route path="/guide" element={<Guide />} />
+                  <Route path="/terms" element={<Terms />} />
+                  <Route path="/privacy" element={<Privacy />} />
 
-                <Route
-                  path="/profile"
-                  element={
-                    <ProtectedRoute>
-                      <Profile />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/admin"
-                  element={
-                    <ProtectedRoute allowedRoles={[ROLES.ADMIN]}>
-                      <AdminPanel />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/supervisor"
-                  element={
-                    <ProtectedRoute allowedRoles={[ROLES.SUPERVISOR]}>
-                      <SupervisorPanel />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/driver"
-                  element={
-                    <ProtectedRoute allowedRoles={DRIVER_ROLES}>
-                      <DriverPanel />
-                    </ProtectedRoute>
-                  }
-                />
-              </Route>
-            </Routes>
-          </GuestPromptProvider>
-        </AuthProvider>
-      </BrowserRouter>
+                  <Route
+                    path="/profile"
+                    element={
+                      <ProtectedRoute>
+                        <Profile />
+                      </ProtectedRoute>
+                    }
+                  />
+                  <Route
+                    path="/admin"
+                    element={
+                      <ProtectedRoute allowedRoles={[ROLES.ADMIN]}>
+                        <AdminPanel />
+                      </ProtectedRoute>
+                    }
+                  />
+                  <Route
+                    path="/supervisor"
+                    element={
+                      <ProtectedRoute allowedRoles={[ROLES.SUPERVISOR]}>
+                        <SupervisorPanel />
+                      </ProtectedRoute>
+                    }
+                  />
+                  <Route
+                    path="/driver"
+                    element={
+                      <ProtectedRoute allowedRoles={DRIVER_ROLES}>
+                        <DriverPanel />
+                      </ProtectedRoute>
+                    }
+                  />
+                </Route>
+              </Routes>
+            </GuestPromptProvider>
+          </AuthProvider>
+        </BrowserRouter>
+      </ToastProvider>
     </ThemeProvider>
   );
 }
